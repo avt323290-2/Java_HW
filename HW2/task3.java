72439ddef93e779721264d216977fa28cd799e4f
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-03-31 17:48:13 +0300

    Добавили условие 3 задачи

---
+++ HW2/task3.java
@@ -0,0 +1,11 @@
+//Task_3.*
+//public class Task3 {
+// 
+// Напишите программу, которая
+// 1) по указанной строке URL скачивает файл и возвращает строковое содержимое файла
+// 2) изменяет строковое содержимое файла подставляя по маске "%s" ваше имя
+// 3) сохраняет файл локально
+// 4) читает сохраненный файл и выводит содержимое в логгер
+// 5) удаляет сохраненный файл
+// public static void main(String[] args) {
+//     String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
