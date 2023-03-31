60b91a21bf31a0da89144f71e0df634607d5c181
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-03-31 18:05:19 +0300

    Добавили условие 3 задачии

--- HW2/task3.java
+++ HW2/task3.java
@@ -9,3 +9,58 @@
 // 5) удаляет сохраненный файл
 // public static void main(String[] args) {
 //     String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String fileUrl = "https://raw.githubusercontent.com/aksodar/JSeminar_2/master/src/main/resources/example.txt";
+        String localFilename = "example.txt";
+
+        // Запрос имени пользователя
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
+
+        Task3 task = new Task3();
+
+        // Скачивание файла
+        String fileContent = task.download(fileUrl, localFilename);
+
+        // Замена содержимого файла
+        String changedContent = task.change(name, fileContent);
+
+        // Сохранение файла локально
+        task.saveOnLocal(localFilename, changedContent);
+
+        // Чтение сохраненного файла и вывод содержимого в логгер
+        task.read(localFilename);
+
+        // Удаление сохраненного файла
+        task.removeFromLocale(localFilename);
+    }
+
+    public String download(String url, String localFilename) {
+        // код для скачивания файла по URL и возврата его содержимого в виде строки
+        return "";
+    }
+
+    public String change(String name, String fileContent) {
+        // код для замены содержимого файла по маске и возврата измененного содержимого в виде строки
+        return "";
+    }
+
+    public void read(String localFilename){
+        // код для чтения содержимого файла и вывода его в логгер
+    }
+
+    public void saveOnLocal(String fileName, String fileContent) {
+        // код для сохранения файла локально
+    }
+
+    public boolean removeFromLocale(String fileName) {
+        // код для удаления сохраненного файла
+        return false;
+    }
+}
--- HW2/task4.java
+++ HW2/task4.java
@@ -1,1 +1,57 @@
 // //Task_4.* К калькулятору из предыдущего дз добавить логирование.
+
+package HW2;
+
+import java.util.Scanner;
+
+public class task_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод первого числа
+        System.out.print("Введите первое число: ");
+        int num1 = scanner.nextInt();
+
+        // Ввод второго числа
+        System.out.print("Введите второе число: ");
+        int num2 = scanner.nextInt();
+
+        // Ввод оператора действия
+        System.out.print("Введите оператор действия (+, -, *, /): ");
+        String operator = scanner.next();
+
+        // Вычисление результата
+        int result = 0;
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Ошибка! Деление на ноль невозможно!");
+                    System.out.println(String.format("Деление на ноль: num1=%d, num2=%d", num1, num2));
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Ошибка! Неверный оператор!");
+                System.out.println(String.format("Неверный оператор: operator=%s", operator));
+                return;
+        }
+
+        // Вывод результата
+        System.out.println("Результат вычисления: ");
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+
+        // Логирование
+        System.out.println("Логирование: ");
+        System.out.println(String.format("num1=%d, operator=%s, num2=%d, result=%d", num1, operator, num2, result));
+    }
+}
