05b9dca174d01e5e67f0135dd4d074f7ddb7d368
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-03-31 17:45:14 +0300

    Добавили решение задачи 3.1

--- HW2/task3.1.java
+++ HW2/task3.1.java
@@ -14,3 +14,26 @@
 //     return "";
 // }
 // }
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Task_3 {
+    private static final Logger logger = Logger.getLogger(Task_3.class.getName());
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число типа byte: ");
+        byte number = scanner.nextByte();
+        scanner.close();
+        try {
+            FileWriter writer = new FileWriter("result.txt");
+            writer.write(Byte.toString(number));
+            writer.close();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Ошибка при записи в файл", e);
+        }
+    }
+}
