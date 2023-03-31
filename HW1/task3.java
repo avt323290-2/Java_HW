6abfe72d7d1b6e8ddfee647f0be08d8de44593d9
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-03-31 18:22:06 +0300

    Добавили решение 3 задачии

--- HW1/task3.java
+++ HW1/task3.java
@@ -1,5 +1,33 @@
-package HW1;
+// Ввод оператора действия
+System.out.print("Введите оператор действия (+, -, *, /): ");
+String operator = scanner.next();
 
-public class task3 {
-    
+// Вычисление результата
+int result = 0;
+switch (operator) {
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    case "*":
+        result = num1 * num2;
+        break;
+    case "/":
+        if (num2 == 0) {
+            System.out.println("Ошибка! Деление на ноль невозможно!");
+            return;
+        }
+        result = num1 / num2;
+        break;
+    default:
+        System.out.println("Ошибка! Неверный оператор!");
+        return;
+}
+
+// Вывод результата
+System.out.println("Результат вычисления: ");
+System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+}
 }
