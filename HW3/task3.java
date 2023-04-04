be7942b392de52a5d4ef9e9da89e6c3675974fb2
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-04-04 13:29:11 +0300

    Добавили решение 3  задачи

---
+++ HW3/task3.java
@@ -0,0 +1,12 @@
+//Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+package HW3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class task3 {
+    public static void main(String[] args) {
+        
+    }
+}
