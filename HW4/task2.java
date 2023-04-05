1b9748d889b5d1700bd2a398393d2de4d401758e
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-04-05 21:56:16 +0300

    Добавили файлы домашнего задания 4

--- HW4/task2.java
+++ HW4/task2.java
@@ -1,5 +1,30 @@
 package HW4;
+/*
+ * Task_4
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Task_4 {
+
+//Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
+// и каждый из их узлов содержит одну цифру.
+// Сложите два числа и верните сумму в виде связанного списка.
+public static void main(String[] args) {
+    Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1,2,3));
+    Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5,4,7));
+    // result [6,6,0,1]
+
+}
+
+
+public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+
+    return new ArrayDeque<>();
+}
+}
+ */
 public class task2 {
     
 }
