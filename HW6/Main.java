f634be7d7d974e6a60c2e9ab19d83528e0650e4a
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-04-13 16:03:10 +0300

    Добавили правки в код

--- HW6/Main.java
+++ HW6/Main.java
@@ -9,6 +9,13 @@ public class Main {
         notebooks.add(new Notebook("Lenovo", 8, 512, "Windows 10", "Black"));
         notebooks.add(new Notebook("HP", 16, 1000, "Windows 10", "Silver"));
         notebooks.add(new Notebook("Dell", 8, 256, "Ubuntu 20.04", "Black"));
+        notebooks.add(new Notebook("Asus zenbook", 32, 6144, "Windows 11", "Black"));
+        notebooks.add(new Notebook("Apple MacBook Air", 16, 1024, "macOS", "Silver"));
+        notebooks.add(new Notebook("Huawei", 16, 3072, "Windows 11", "Silver"));
+        notebooks.add(new Notebook("Asus", 16, 512, "Windows 11", "Blue"));
+        notebooks.add(new Notebook("HP", 16, 512, "Windows 10", "White"));
+        notebooks.add(new Notebook("Dell XPS 13", 16, 512, "Windows 10", "Black"));
+
         // Запрашиваем у пользователя критерии фильтрации
     Scanner scanner = new Scanner(System.in);
     Map<String, Object> filters = new HashMap<>();
